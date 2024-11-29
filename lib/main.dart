6b75b6c2afcd1def395bd7da9d5775e5ff4cce97
Ahmad Mario <riotheassassin@gmail.com>
2024-11-29 14:35:02 +0700

    Change navigation method & Add class for second page (HomePage) with tab

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '2210130001 - Ahmad Marioale Sulolipu - Assessment 2',
+      title: 'title',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 255, 255, 255)),
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: const LoginPage(title: 'Demo Login')
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LoginPage(title: 'Demo Login'),
+        '/home': (context) => const HomePage(),
+      },
     );
   }
 }
@@ -29,53 +33,100 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
         title: Text(widget.title),
+        titleTextStyle: const TextStyle(color: Colors.white)
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            SizedBox(height: 10,),
-            TextField(
+            const SizedBox(height: 10,),
+            const TextField(
               decoration: InputDecoration(
                 icon: Icon(Icons.account_circle),
                 border: OutlineInputBorder(),
                 hintText: 'Nama',
               ),
             ),
-            SizedBox(height: 10,),
-            TextField(
+            const SizedBox(height: 10,),
+            const TextField(
               decoration: InputDecoration(
                 icon: Icon(Icons.phone),
                 border: OutlineInputBorder(),
                 hintText: 'No. HP',
               ),
             ),
-            SizedBox(height: 10,),
-            TextField(
+            const SizedBox(height: 10,),
+            const TextField(
               decoration: InputDecoration(
                 icon: Icon(Icons.email),
                 border: OutlineInputBorder(),
                 hintText: 'Email',
               ),
             ),
-            SizedBox(height: 10,),
+            const SizedBox(height: 10,),
             TextButton(
-              style: ButtonStyle(
+              style: const ButtonStyle(
                 foregroundColor: WidgetStatePropertyAll<Color>(Colors.white),
                 backgroundColor: WidgetStatePropertyAll<Color>(Colors.blue)
               ),
-              onPressed: null, 
-              child: Text("Validasi Data"))
+              onPressed: () {
+                Navigator.pushNamed(context, '/home');
+              }, 
+              child: const Text("Validasi Data")
+            ),
           ],
         ),
       ),
     );
   }
 }
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+            bottom: const TabBar(
+              labelStyle: TextStyle(color: Colors.white),
+              unselectedLabelStyle: TextStyle(color: Colors.white),
+              tabs: [
+                Tab(text: 'Home'),
+                Tab(text: 'Product'),
+                Tab(text: 'Contact')
+              ]
+            ),
+            title: const Text('Assessment 2'),
+            leading: const IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: null,
+            ),
+            titleTextStyle: const TextStyle(color: Colors.white),
+          ),
+          body: const TabBarView(
+            children: [
+              Center(child: Text('Home Page')), 
+              Center(child: Text('Product')), 
+              Center(child: Text('Contact'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+// Icon(Icons.home),
+// Icon(Icons.add_shopping_cart),
+// Icon(Icons.phone),
